de0692622ad0285e7850427b89d167f019dd0043
Mariano Rapaport <marianr99@outlook.com>
2020-09-16 15:10:53 -0300

    simple upd

--- src/main/java/com/example/streamrabbit/StreamRabbitApplication.java
+++ src/main/java/com/example/streamrabbit/StreamRabbitApplication.java
@@ -2,7 +2,11 @@ package com.example.streamrabbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class StreamRabbitApplication {
 
@@ -10,4 +14,9 @@ public class StreamRabbitApplication {
 		SpringApplication.run(StreamRabbitApplication.class, args);
 	}
 
+	@StreamListener(target = Sink.INPUT)
+	public void processMeals(String meal){
+
+		System.out.println("Recibí esta orden: " + meal);
+	}
 }
